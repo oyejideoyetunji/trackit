57aba50fefca79ab7f69be27d25fda16ea2c8f4c
oluwatobi oyejide <moyejide2016@gmail.com>
2020-07-18 06:02:02 +0100

    added edit todo feature

--- js/script.js
+++ js/script.js
@@ -9,12 +9,14 @@ const todoDate = todoForm['date'];
 const todoTab = document.getElementById('todo-tab');
 const doneTab = document.getElementById('done-tab');
 const addTodoTab = document.getElementById('add-todo-tab');
+// const submitButton = document.getElementById('submit-btn');
 
 const todoTableBlock = document.getElementById('todo-table-wrp');
 const todoTable = document.getElementById('todo-table');
 const doneTableBlock = document.getElementById('done-table-wrp');
 const doneTable = document.getElementById('done-table');
 const addTodoBlock = document.getElementById('add-todo-block');
+// const formButtonBlock = document.getElementById('frm-btn-blck');
 const doneStatusBar = document.getElementById('done-staus');
 
 
@@ -42,6 +44,7 @@ todoForm.addEventListener('submit', (event)=>{
     getTodoData();
     resetTodoFormValues();
     createTodoTable();
+    showTodoList();
 });
 
 
@@ -67,12 +70,24 @@ function showDoneList(){
         doneStatusBar.style.display = 'none';
 }
 
+function hasDuplicate(element, array){
+    if(array.length < 1) return false;
+    let elementIndex = array.findIndex((item) =>{
+        return (item.title == element.title && item.date == element.date);
+    });
+
+    return elementIndex < 0 ? false : true;
+}
+
 function getTodoData(){
     let todoObj = {};
     if(todoTitle.value == '' || todoDate.value == '') return;
     todoObj.title = todoTitle.value;
     todoObj.date = todoDate.value;
-    todoListArray.push(todoObj);
+    hasDuplicate(todoObj, todoListArray) ?
+        alert("Todo already exist") :
+        todoListArray.push(todoObj);
+    return
 }
 
 function resetTodoFormValues(){
@@ -114,8 +129,6 @@ function createDoneTable(){
             (doneListArray.length < 1) ?
             doneStatusBar.style.display = 'block' :
             doneStatusBar.style.display = 'none';
-
-            console.log(todoListArray, doneListArray);
         });
     });
 }
@@ -130,24 +143,37 @@ function createTodoTable(){
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
         let p3 = document.createElement('p');
-        let btn = document.createElement('button');
-        btn.setAttribute('class', 'bg-blue-var tab');
+        let btn1 = document.createElement('button');
+        let btn2 = document.createElement('button');
+        btn1.setAttribute('class', 'bg-blue-var tab title-lft');
+        btn2.setAttribute('class', 'bg-blue-var tab title-rt');
 
         let txt1 = document.createTextNode(todoData.title);
         let txt2 = document.createTextNode(`On/Before: ${todoData.date}`);
-        let txt3 = document.createTextNode('move to done');
+        let txt3 = document.createTextNode('edit Todo');
+        let txt4 = document.createTextNode('move to completed');
 
         p1.appendChild(txt1);
         p2.appendChild(txt2);
-        btn.appendChild(txt3);
-        p3.appendChild(btn);
+        btn1.appendChild(txt3);
+        btn2.appendChild(txt4);
+        p3.appendChild(btn1);
+        p3.appendChild(btn2);
 
         row.appendChild(p1);
         row.appendChild(p2);
         row.appendChild(p3);
         todoTable.appendChild(row);
 
-        btn.addEventListener('click', (event)=>{
+        btn1.addEventListener('click', (event)=>{
+            todoTitle.value = todoData.title;
+            todoDate.value = todoData.date;
+            // let updateButton = document.createElement('button');
+            showAddTodoBlock();
+            todoListArray.splice(todoListArray.indexOf(todoData), 1);
+        })
+
+        btn2.addEventListener('click', (event)=>{
             doneListArray.push(todoData);
             todoListArray.splice(todoListArray.indexOf(todoData), 1);
             row.parentNode.removeChild(row);
